9ca0f78661c6fd2b960a4f9dde412730b11c65e8
Simon Bell <bimonsell@gmail.com>
2023-06-08 18:44:43 +0700

    Initial Inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const initialScreens = [
+    {
+      heading: 'Today, I should:',
+      id: 'one',
+    },
+    {
+      heading: `Then, it'd be nice to:`,
+      id: 'two',
+    },
+    {
+      heading: `And finally, I could:`,
+      id: 'three',
+    },
+  ];
+  const [currentInitialScreen, setCurrentInitialScreen] = useState(0);
+
+  const [task, setTask] = useState('');
+
+  const storageTask = localStorage.getItem(
+    `Task ${initialScreens[currentInitialScreen].id}`
+  );
+
+  useEffect(() => {
+    console.log('useEffect - storageTask:', storageTask);
+    if (storageTask) {
+      setTask(storageTask);
+    }
+  }, [storageTask]);
+
+  const handleTaskInputChange = (e) => {
+    console.log('handleTaskInputChange - e.target.value:', e.target.value);
+    setTask(e.target.value);
+  };
+
+  const handleSaveTask = () => {
+    console.log('handleSaveTask - task:', task);
+    if (task.length > 0) {
+      localStorage.setItem(
+        `Task ${initialScreens[currentInitialScreen].id}`,
+        task.trim()
+      );
+      setTask(task.trim());
+    } else {
+      localStorage.removeItem(
+        `Task ${initialScreens[currentInitialScreen].id}`
+      );
+      console.log('handleSaveTask - task:', task);
+    }
+  };
+
+  console.log('Rendering App - currentInitialScreen:', currentInitialScreen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> {initialScreens[currentInitialScreen].heading} </h1>
+      <input
+        className='initial-input'
+        type='text'
+        autoComplete='off'
+        maxLength='17'
+        spellCheck='false'
+        value={task}
+        onBlur={handleSaveTask}
+        onChange={handleTaskInputChange}
+        id={`start-input-${initialScreens[currentInitialScreen].id}`}
+      />
+
+      <button
+        onClick={() => setCurrentInitialScreen(currentInitialScreen - 1)}
+        disabled={currentInitialScreen === 0}
+      >
+        BACK
+      </button>
+      <button
+        onClick={() => setCurrentInitialScreen(currentInitialScreen + 1)}
+        disabled={currentInitialScreen === initialScreens.length - 1}
+      >
+        NEXT
+      </button>
     </div>
   );
 }
