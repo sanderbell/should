c828a99d1b38504a43b63a0c5c89084efccfc3b6
Simon Bell <bimonsell@gmail.com>
2023-06-08 22:59:39 +0700

    Initial Screens Ready

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const initialScreens = [
+  let oldVisitor = JSON.parse(localStorage.getItem('Old visitor'))
+    ? true
+    : false;
+
+  localStorage.setItem('Old visitor', true);
+
+  console.log('OldVisitor is', oldVisitor);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const screens = [
     {
       heading: 'Today, I should:',
       id: 'one',
@@ -12,22 +29,44 @@ function App() {
       id: 'two',
     },
     {
-      heading: `And finally, I could:`,
+      heading: `Finally, I could:`,
       id: 'three',
     },
+
+    {
+      heading: `Finally, I could:`,
+      id: 'four',
+    },
+
+    {
+      heading: `Finally, I could:`,
+      id: 'five',
+    },
+
+    {
+      heading: `Finally, I could:`,
+      id: 'six',
+    },
+
+    {
+      heading: `Yay! All done for today!`,
+      id: 'seven',
+    },
   ];
+
   const [currentInitialScreen, setCurrentInitialScreen] = useState(0);
 
   const [task, setTask] = useState('');
 
   const storageTask = localStorage.getItem(
-    `Task ${initialScreens[currentInitialScreen].id}`
+    `Task ${screens[currentInitialScreen].id}`
   );
 
   useEffect(() => {
-    console.log('useEffect - storageTask:', storageTask);
     if (storageTask) {
       setTask(storageTask);
+    } else {
+      setTask('');
     }
   }, [storageTask]);
 
@@ -36,51 +75,104 @@ function App() {
     setTask(e.target.value);
   };
 
+  const placeholder = [
+    'Run 10 miles',
+    'Buy some durian',
+    'Go to the gym',
+    'Read 50 pages',
+    'Call my brother',
+  ];
+
+  const getRandomPlaceholder = () => {
+    const randomIndex = Math.floor(Math.random() * placeholder.length);
+    return placeholder[randomIndex];
+  };
+
   const handleSaveTask = () => {
     console.log('handleSaveTask - task:', task);
     if (task.length > 0) {
       localStorage.setItem(
-        `Task ${initialScreens[currentInitialScreen].id}`,
+        `Task ${screens[currentInitialScreen].id}`,
         task.trim()
       );
       setTask(task.trim());
     } else {
-      localStorage.removeItem(
-        `Task ${initialScreens[currentInitialScreen].id}`
-      );
+      localStorage.removeItem(`Task ${screens[currentInitialScreen].id}`);
       console.log('handleSaveTask - task:', task);
     }
   };
 
   console.log('Rendering App - currentInitialScreen:', currentInitialScreen);
 
+  localStorage.setItem('Old visitor', true);
+
   return (
     <div className='App'>
-      <h1> {initialScreens[currentInitialScreen].heading} </h1>
-      <input
-        className='initial-input'
-        type='text'
-        autoComplete='off'
-        maxLength='17'
-        spellCheck='false'
-        value={task}
-        onBlur={handleSaveTask}
-        onChange={handleTaskInputChange}
-        id={`start-input-${initialScreens[currentInitialScreen].id}`}
-      />
-
-      <button
-        onClick={() => setCurrentInitialScreen(currentInitialScreen - 1)}
-        disabled={currentInitialScreen === 0}
+      {(modalVisible || oldVisitor === false) && (
+        <div className='modal'>
+          <h2 className='modal-heading'>Welcome to should:</h2>
+          <p className='modal-text'>
+            This is how it works: worksworksworksw
+            <br />
+            sworksworksworksworkswsworksw
+            <br />
+            sworksworkswoorksworksw
+            <br />
+            sworksworkswosworksworksworksworksw
+            <br />
+            sworksworksworksworkswsworksw
+            <br />
+            sworks workswoorksworksw
+            <br />
+            sworksworkswosworks
+          </p>
+          <button onClick={closeModal}>✖️</button>
+        </div>
+      )}
+      <div
+        className={modalVisible ? 'initial-content blurred' : 'initial-content'}
       >
-        BACK
-      </button>
-      <button
-        onClick={() => setCurrentInitialScreen(currentInitialScreen + 1)}
-        disabled={currentInitialScreen === initialScreens.length - 1}
-      >
-        NEXT
-      </button>
+        <h1> {screens[currentInitialScreen].heading} </h1>
+        <input
+          className='initial-input'
+          type='text'
+          autoComplete='off'
+          maxLength='17'
+          spellCheck='false'
+          placeholder={getRandomPlaceholder()}
+          value={task}
+          onBlur={handleSaveTask}
+          onChange={handleTaskInputChange}
+          id={`start-input-${screens[currentInitialScreen].id}`}
+        />
+        <div id='buttons-container'>
+          {currentInitialScreen > 0 ? (
+            <button
+              onClick={() => setCurrentInitialScreen(currentInitialScreen - 1)}
+              disabled={currentInitialScreen === 0}
+            >
+              ←
+            </button>
+          ) : (
+            <button onClick={openModal}>ℹ</button>
+          )}
+          {currentInitialScreen < 2 ? (
+            <button
+              onClick={() => setCurrentInitialScreen(currentInitialScreen + 1)}
+              disabled={task.length === 0}
+            >
+              →
+            </button>
+          ) : (
+            <button
+              onClick={() => setCurrentInitialScreen(currentInitialScreen + 1)}
+            >
+              🙃
+            </button>
+          )}
+        </div>
+      </div>
+      <div className='final-content'>dfsdfdsf</div>
     </div>
   );
 }
